521da0c908171688efc28bcb23f9d4f06dd340b4
Patrick Marques <patrickmarques2009@hotmail.com>
2024-06-25 21:16:38 -0300

    Criação do projeto react com vite e bootsrap

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Site de Previsão do Tempo</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
